de832ca761ed573a32b0f2b620e36b6713a6c6d3
marcodeiulio <mdeiulio@icloud.com>
2022-01-20 17:05:10 +0100

    - Added script2.js

---
+++ js/script2.js
@@ -0,0 +1,43 @@
+/* 
+# Snack2
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
+Infine, usando la destrutturazione, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+| BONUS
+Stampare in pagina oltre che in console!
+*/
+
+//# Functions
+const cs = (argument) => {
+	return console.log(argument);
+}
+const ct = (argument) => {
+	return console.table(argument);
+}
+
+const randNum = (min, max) => {
+	return Math.floor(Math.random() * max - min + 1) + min;
+}
+
+//# 
+const teams = [
+	{ name: 'Bayern Monaco', pts: 0, foulsDrawn: 0 },
+	{ name: 'AFC Richmond', pts: 0, foulsDrawn: 0 },
+	{ name: 'Sassuolo', pts: 0, foulsDrawn: 0 },
+	{ name: 'Atalanta', pts: 0, foulsDrawn: 0 },
+];
+
+for (let i = 0; i < teams.length; i++) {
+	teams[i].pts = randNum(0, 25);
+	teams[i].foulsDrawn = randNum(0, 10);
+}
+ct(teams);
+
+const teamsFouls = [];
+for (let i = 0; i < teams.length; i++) {
+	const { name, pts } = teams[i];
+	teamsFouls.push({ name, pts });
+}
+
+ct(teamsFouls);
